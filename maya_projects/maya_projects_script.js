3796994afb786c7af69213f0e45bd77063456b4f
amcababat <amcababat@ucdavis.edu>
2017-06-01 10:20:09 -0700

    maya projjects

--- maya_projects/maya_projects_script.js
+++ maya_projects/maya_projects_script.js
@@ -13,6 +13,17 @@ document.addEventListener("DOMContentLoaded", function (event) {
     close_1.addEventListener('click', function () {
         img_1.style.display = "none";
     })
+    var proj_2 = document.getElementById('proj_2');
+    var img_2 = document.getElementById('img_2');
+    var close_2 = document.getElementById('close_2');
 
-    // add a cursor hover
+    proj_2.addEventListener('click', function () {
+        img_2.style.display = "block";
+        // add a fade up?
+    })
+
+    close_2.addEventListener('click', function () {
+            img_2.style.display = "none";
+        })
+        // add a cursor hover
 });
