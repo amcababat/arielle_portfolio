19acf9305812006eb7ae6166dbc5ae73c147c53f
amcababat <amcababat@ucdavis.edu>
2017-06-01 11:01:58 -0700

    new design button

--- designs/designs_script.js
+++ designs/designs_script.js
@@ -27,6 +27,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
     close_1.addEventListener('click', function () {
         paragraph_1.style.display = "none";
     })
+
     var code = document.getElementById('code');
     var paragraph_3 = document.getElementById('paragraph_3');
     var close_3 = document.getElementById('close_3');
@@ -37,7 +38,20 @@ document.addEventListener("DOMContentLoaded", function (event) {
     })
 
     close_3.addEventListener('click', function () {
-            paragraph_3.style.display = "none";
+        paragraph_3.style.display = "none";
+    })
+
+    var project_4 = document.getElementById('project_4');
+    var paragraph_4 = document.getElementById('paragraph_4');
+    var close_4 = document.getElementById('close_4');
+
+    project_4.addEventListener('click', function () {
+        paragraph_4.style.display = "block";
+        // add a fade up?
+    })
+
+    close_4.addEventListener('click', function () {
+            paragraph_4.style.display = "none";
         })
         // add a cursor hover
 });
